9abcbeefa91a2b4bd6f2ff22e8e1571218878b1a
Christopher Thomson <christopher.thomson42@gmail.com>
2017-08-29 13:16:41 -0700

    started building tests, added travis

---
+++ src/reciver.store.test.js
@@ -0,0 +1,9 @@
+import reducer from './salutation.reducer';
+import { ADD_GREETING, ADD_NAME, RESET } from '../containers/salutation.constants';
+import { addGreeting, addName, reset} from './actions/index';
+
+describe('counter', () => {
+
+    describe('actions, ')
+
+});
