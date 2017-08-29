6ba2006ad314b1cd01b998a491ccc1c3a9592d65
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-08-29 10:41:15 -0700

    fix reset for forms

--- src/Greeter.js
+++ src/Greeter.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {updateSalutation, updateName, reset} from './greeter.actions';
 
-export function Greeter({salutation, name, dispatch}) {
+export function Greeter({salutation = '', name = '', dispatch}) {
     return (
         <div>
             <span>{salutation}, </span>
             <span>{name}</span>!
             <p>Salutation: </p>
-            <input onChange={({target}) => dispatch(updateSalutation(target.value))}></input>
+            <input value={salutation} onChange={({target}) => dispatch(updateSalutation(target.value))}></input>
             <p>Name: </p>
-            <input onChange={({target}) => dispatch(updateName(target.value))}></input>
+            <input value={name} onChange={({target}) => dispatch(updateName(target.value))}></input>
             <br/>
             <button onClick={() => dispatch(reset())}>Reset</button>
         </div>
